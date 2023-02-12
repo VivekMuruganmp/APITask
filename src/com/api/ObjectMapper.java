f4d90acd9e79878f1b80d7607079fd6042d67e7f
VivekMuruganmp <vivekmechanical307@gmail.com>
2023-02-12 17:40:34 +0530

    Rolex Spin Off

--- src/com/api/ObjectMapper.java
+++ src/com/api/ObjectMapper.java
@@ -1,5 +1,15 @@
 package com.api;
 
 public class ObjectMapper {
-
+	
+	
+	private void getFrames() {
+		int a = 24;
+		int b= 55;
+	}
+	
+public static void main(String[] args) {
+	System.out.println();
+	
+}
 }
